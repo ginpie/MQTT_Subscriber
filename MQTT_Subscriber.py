a4c486c3a4f41b49b5ce36239d39d4740a170f68
Jinpei Chen <jinpei.chen@anu.edu.au>
2020-06-05 00:27:09 +1000

    Update MQTT_Subscriber.py

--- MQTT_Subscriber.py
+++ MQTT_Subscriber.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import time
+import csv
 
 SERVER = "comp3310.ddns.net"
 PORT = 1883
@@ -8,6 +9,9 @@ PASSWORD = "33106331"
 CLIENT_ID="3310-u6743886"
 SLOW = ["counter/slow/q0", "counter/slow/q1", "counter/slow/q2"]
 FAST = ["counter/fast/q0", "counter/fast/q1", "counter/fast/q2"]
+ALL = SLOW + FAST
+list = []
+previous_time = time.time()
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -16,34 +20,55 @@ def on_connect(client, userdata, flags, rc):
       print("Successfully connected with code: " + str(rc))
    else:
       print("Error with code: " + str(rc))
-   # Subscribing in on_connect() means that if we lose the connection and
-   # reconnect then subscriptions will be renewed.
+
 
 def on_disconnect(clietn, userdata, rc):
    print("client disconnected ok")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
+   this_time = time.time()
+   global previous_time 
+   gap = round((this_time - previous_time) * 1000000)
+   previous_time = this_time
+   ms = (msg.topic, msg.payload, gap)
+   list.append(ms)
    print( msg.topic + " " + str(msg.payload) )
 
 def on_log(client, userdata, level, buf):
-   print("log: " + buf)
+   print("log: " + buf )
 
 def on_publish(client, userdata, mid):
    print("In on_pub callback mid= ", mid)
 
+def writeCSV(csvfile):
+   writ = csv.writer(csvfile)
 
-client = mqtt.Client(CLIENT_ID)
-client.on_connect = on_connect
-client.on_message = on_message
-# client.on_log = on_log
+# subscribe to one of the topics at a time
+for i in range(5,6):
+   
+   client = mqtt.Client(CLIENT_ID)
+   client.on_connect = on_connect
+   client.on_message = on_message
+   # client.on_log = on_log
+   client.username_pw_set(USERNAME, PASSWORD)
+   client.connect(SERVER, PORT)
 
-client.username_pw_set(USERNAME, PASSWORD)
-client.connect(SERVER, PORT)
+   list.append(("qos", i))
+   topic = ALL[i]
+   lv = i%3
+   client.subscribe(topic, qos=lv)
+   client.subscribe("$SYS/#")
+   
+   client.loop_start()
+   time.sleep(300)
+   client.loop_stop()
+   
+   client.unsubscribe(topic)
+   
+client.disconnect()
 
-client.subscribe(SLOW[0], 0)
-
-time.sleep(0)
-# client.disconnect
-client.loop_forever()
-
+with open('sys_trial4.csv', 'w', newline='') as csvfile:
+   writ = csv.writer(csvfile)
+   for i in range(0, len(list)):
+      writ.writerow(list[i])
